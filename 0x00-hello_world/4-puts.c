31b7fd3c6adcbbf98bc101e30cccdbffbab21430
Olayemi G. Obafuwa <olayemijacob@gmail.com>
2022-09-08 04:18:53 -0700

    for main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-/* 
- * This user program prints exactly the string
+/*
+ * main - This user program prints exactly the string.
+ * main - Entry point
  * "Programming is like building a multilingual puzzle"
  * followed by a new line
  * return: always (0) sucess.
