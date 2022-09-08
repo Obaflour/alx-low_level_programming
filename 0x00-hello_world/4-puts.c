c72dd244cfc4fcd5f4598c73acc9a6f684a8f6b1
Olayemi G. Obafuwa <olayemijacob@gmail.com>
2022-09-08 04:00:13 -0700

    go

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
  * This user program prints exactly the string
  * "Programming is like building a multilingual puzzle"
  * followed by a new line
- * return: always (0) sucess
+ * return: always (0) sucess.
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle")
-		return (0)
+	puts("\"programming is like building a multilingual puzzle");
+		return (0);
 }
