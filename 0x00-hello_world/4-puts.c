f49c9ed8b94aee4139d9f84cbbe90037feddd10c
Olayemi G. Obafuwa <olayemijacob@gmail.com>
2022-09-08 03:40:18 -0700

    test it

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <studio.h>
+/* 
+ * This user program prints exactly the string
+ * "Programming is like building a multilingual puzzle"
+ * followed by a new line
+ * return: always (0) sucess
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle")
+		return (0)
+}
