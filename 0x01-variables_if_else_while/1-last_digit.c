3341724aff0ae24cc9ed69e70af0141138b08e4b
Olayemi G. Obafuwa <olayemijacob@gmail.com>
2022-09-09 07:11:46 -0700

    computer

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	int n, last_digit;
 
 		srand(time(0));
-	n = rand(0 - RAND_MAX / 2);
+	n = rand() - RAND_MAX / 2;
 			last_digit = n % 10;
 			printf("Last digit of %d is *d and is ", n, last_digit);
 			if (last_digit > 5)
