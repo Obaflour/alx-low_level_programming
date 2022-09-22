1351aa2cb5fb63a4ac10c771fba0067eea61c151
Olayemi G. Obafuwa <olayemijacob@gmail.com>
2022-09-22 02:11:31 -0700

    number 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,39 +14,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, r;
+	int i = 0;
 
-	/**
-	 * iterate through src and
-	 * compare it with dest
-	 */
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while ((s1[i] == s2[i]) && (s1[i] != '\0'))
 	{
-		/**
-		 * if they differ by a single character
-		 * don't iterate further
-		 */
-		if (s1[i] != s2[i])
-		{
-			/**
-			 * if they differ by a single character
-			 * don't iterate further
-			 */
-			if (s1[i] != s2[i])
-			{
-				/**
-				 * return the difference between
-				 * the two characters
-				 */
-				r = s1[i] - s2[i];
-				break;
-			}
-			else
-			{
-				r = s1[i] - s2[i];
-			}
-			i++;
-		}
-
-		return (r);
+		i++;
 	}
+	return (s1[i] - s2[i]);
+}
