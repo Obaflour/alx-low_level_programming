9a62e0916196959b2f617cf969846ee2caebc43d
Olayemi G. Obafuwa <olayemijacob@gmail.com>
2022-09-22 09:29:25 -0700

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,8 @@ char *leet(char *str);
 char *rot13(char *str);
 void print_number(int n);
 int _putchar(char c);
+int main(void);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
