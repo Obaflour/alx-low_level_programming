544eda5c16fe4aae68d0f6cb2105a2bee2db4852
Olayemi G. Obafuwa <olayemijacob@gmail.com>
2022-09-26 06:50:53 -0700

    look up

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _strstr - first occurrence of the substring needle in the string
+ * _strstr - first occurrence of the substring needle in the string haystack
  * @haystack: main str to be examined
  * @needle: searched in haystack
- * Return: Always 0
+ * Return: return 0
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	char *str1, *str2; /*declaring variables*/
+	char *str1, *str2; /*Declaring variables*/
 
 	while (*haystack != '\0')
 	{
