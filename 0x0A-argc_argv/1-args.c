fe016333c2f22159590e75983a4f18c1b0c1e44c
Olayemi G. Obafuwa <olayemijacob@gmail.com>
2022-09-30 09:50:19 -0700

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,7 @@
  * main - check the code for ALX student.
  * @argv: The character to print
  * @argc: The character to print
- * Retur: Always 0.
+ * Return: Always 0.
  */
 
 int main(int argc, char *argv[])
