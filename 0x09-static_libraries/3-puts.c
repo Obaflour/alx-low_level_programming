8305829ce23686c44212e12aa617759e005e09ef
Olayemi G. Obafuwa <olayemijacob@gmail.com>
2022-09-30 12:37:23 -0700

    Task 0

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,18 +1,19 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * _puts -check if a number is greater than 0
- * @str: The number to be checked
+ * _puts - prints a string, followed by a newline to stdout
+ *
+ * @str: string parameter to print
+ *
+ * Return: Nothing
  */
 
 void _puts(char *str)
 {
-	int i;
-
-	for (i = 0; *str != '\0'; i++)
+	while (*str != '\0')
 	{
-		_putchar(*str);
-		str++;
+		_putchar(*str + 0);
+		++str;
 	}
 	_putchar('\n');
 }
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -4,12 +4,11 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  *
- * Return: Always return 1 on success.
+ * Return: Always 1 on success
  * on error, -1 is returned, and erro is set appropriately.
- *
  */
 
-int _putcar(char c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
