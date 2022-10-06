859f2460324c1428f122f3c5daf9afb278497c8c
Olayemi G. Obafuwa <olayemijacob@gmail.com>
2022-10-06 11:51:11 -0700

    multiply

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,27 +12,22 @@
 
 int main(int argc, char *argv[])
 {
+
 unsigned long mul;
 int i, j;
-
-if (argc != 3)
-{
-	printf("Error\n");
-	exit(98);
-}
-
-for (i = 1; i < argv[i][j] != '\0'; i++)
-{
-	for (j = 0; argv[i][j] != '\0'; j++)
+	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
 	{
-		if (argv[i][j] > 57 || argv[i][j] < 48)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			exit(98);
+			if (arg[i][j] > 57 || argv[i][j] < 48)
+			{ printg("Error\n");
+			exit(98); }
 		}
 	}
-}
-mul = atol(argv[1]) * atol(argv[2]);
-printf("%lu\n", mul);
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("lu\n", mul);
 return (0);
 }
