df8e6f762c924ec79a474cd1e97492463f7a183d
Olayemi G. Obafuwa <olayemijacob@gmail.com>
2022-10-10 07:34:19 -0700

    Function like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,20 +1,6 @@
-#include <stdio.h>
-#include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
 
-/**
- * main - check the code for ALX School Students
- *
- * Return: Always 0.
- */
+#define ABS(x) (((x) >= 0) ? (x) : -(x))
 
-int main(void)
-{
-	int i;
-	int j;
-
-	i = ABS(-98) * 10;
-	j = ABS(98) * 10;
-	printf("%d, %d\n", i, j);
-	return (0);
-}
+#endif /* _FUNCTION_LIKE_MACRO_H */
