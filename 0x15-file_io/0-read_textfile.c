eeb69a75841a1878629023504157eb8bcae7eee9
Olayemi G. Obafuwa <olayemijacob@gmail.com>
2022-10-31 06:46:12 -0700

    0x15

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -26,6 +26,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buf);
 		return (0);
 	}
+	buf[i] = '\0';
+	close(fd);
+	y = write(STDOUT_FILENO, buf, i);
+	if (y < 0)
+	{
+		free(buf);
+		return (0);
+	}
 	free(buf);
 	return (y);
 }
