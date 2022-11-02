b1f57824ba6314c2b49fdc3d7102a6f6b1930ec0
Olayemi G. Obafuwa <olayemijacob@gmail.com>
2022-11-02 10:31:04 -0700

    Task 3 corrected

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	fd_r = open(argv[1], O_RDONLY);
 	if (fd_r < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
@@ -28,14 +28,14 @@ int main(int argc, char *argv[])
 	{
 		if (fd_w < 0 || write(fd_w, buf, x) != x)
 		{
-			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_r);
 			exit(99);
 		}
 	}
 	if (x < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	m = close(fd_r);
@@ -43,9 +43,9 @@ int main(int argc, char *argv[])
 	if (m < 0 || n < 0)
 	{
 		if (m < 0)
-			dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd_r);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
 		if (n < 0)
-			dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd_w);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
 		exit(100);
 	}
 	return (0);
